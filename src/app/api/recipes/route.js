23a30081e35d4b5dfd79319860c00e553cf29307
Lennart Frese <lennart.frese@tum.de>
2023-11-18 18:40:23 +0100

    jo läuft. weiß ich nicht vielleihttttttttt

--- src/app/api/recipes/route.js
+++ src/app/api/recipes/route.js
@@ -1,19 +1,24 @@
 import { NextResponse } from "next/server.js";
 
 import lib from "../../../sessionManagement.js";
+import { getBest } from "../../../matching.js ";
 
 export async function GET(req) {
 
   const existingSessionId = req.cookies.get("session_id");
+  const skip = Number(req.nextUrl.searchParams.get('skip')) || 0;
+  const take = Number(req.nextUrl.searchParams.get('skip')) || 20;
 
-  const db = getDB();
+  const db = JSON.parse(JSON.stringify(getDB().copy));
   const session = mapUser(existingSessionId);
   const { filters, preferences } = session;
 
   // session.filters
   // session.preferences
+  getBest(0,0,session,db);
 
 
-
-  return response;
+  return NextResponse.json({
+      recipes : "empty"
+  });
 }
